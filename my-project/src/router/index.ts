f2a1a57a13d9d633feed213fbf5b118383a75264
victorchacon <victor@impulsatalentum.org>
2025-03-10 15:47:00 +0100

    feat:ProductListPage created

--- my-project/src/router/index.ts
+++ my-project/src/router/index.ts
@@ -1,19 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
-import PerfilsView from "../components/perfils/perfilsView.vue";
-import ReclutatsView from "../components/perfils/reclutatsView.vue";
 import ProductListPage from "../components/ProductListPage.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-    path: "/perfils",
-      name: "perfils",
-      component: PerfilsView,
-    },{
-    path: "/reclutats",
-      name: "reclutats",
-      component: ReclutatsView,
-    },
     {
     path: "/",
       name: "productos",
